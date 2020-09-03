2041da9a06643d1480cf18d7da50d2e164150fc6
Yunhao <yz2327@cornell.edu>
2020-09-03 16:04:26 -0400

    update conf for baseline hotstuff

--- hotstuff/deploy/exp1-baseline-azure/single_server.sh
+++ hotstuff/deploy/exp1-baseline-azure/single_server.sh
@@ -5,6 +5,5 @@ base_dir="./deploy/exp1-baseline-azure"
 # original hotstuff only one instance
 echo "    starting baseline replica $1"
 
-# print to screen
-./examples/hotstuff-app $base_dir/conf/hotstuff.conf --conf $base_dir/conf/hotstuff-sec$1.conf  2>&1 &
+./examples/hotstuff-app $base_dir/conf/hotstuff.gen.conf --conf $base_dir/conf/hotstuff.gen-sec$1.conf > /dev/null 2>&1 &
 
