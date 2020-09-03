66a181e852d918e33fe44951760c0039e118b00a
Yunhao <yz2327@cornell.edu>
2020-09-03 18:13:32 -0400

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ then
     echo "Usage: deploy.sh {username}"
 
 else
+    # remove old log and data
+    rm hotstuff/deploy/*/log/*
+    rm hotstuff/deploy/*/data/*
+    
     cat hosts | while read machine
     do
         echo "deploy code on machine ${machine}"
